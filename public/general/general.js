e10dc8ddb13dc0cf82971a85d113c83dd75b8deb
Pedro <pedromonrealsanchez@gmail.com>
2024-07-18 12:00:44 +0200

    a

--- public/general/general.js
+++ public/general/general.js
@@ -43,7 +43,7 @@ document.head.appendChild(fontLink);
 // Función para obtener el número de jugadores conectados
 function numeroJugadores() {
     if (typeof MinecraftAPI !== 'undefined') {
-        MinecraftAPI.getServerStatus('Goldencraftbeta.aternos.me', {
+        MinecraftAPI.getServerStatus('goldencraftbeta.aternos.me', {
             port: 25565
         }, function (err, status) {
             if (err) {
