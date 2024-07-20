fdcd761bcfc995be0c4c114f7f72ed9ee26916cc
Pedro <pedromonrealsanchez@gmail.com>
2024-07-20 02:07:51 +0200

    registro

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const User = require('./modelos/user');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const port = 80;
 
 // Middleware para procesar solicitudes JSON
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
 
 // Middleware para servir archivos estáticos desde el directorio 'public'
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Conexión a la base de datos en MongoDB Atlas
 const mongoURL = 'mongodb+srv://goldencraftWEB:GoldencraftAdmins%40@goldencraft.uoswo3m.mongodb.net/mydatabase';
 
-// Importar las rutas para las noticias
+// Importar las rutas para las noticias y preguntas frecuentes
 const rutasNoticias = require('./rutas/noticias.js');
 const rutasPreguntasFrecuentes = require('./rutas/preguntasFrecuentes.js');
 
@@ -28,13 +28,68 @@ app.use('/api', rutasPreguntasFrecuentes);
 mongoose.connect(mongoURL)
     .then(() => {
         console.log('Conectado a MongoDB');
-        // Iniciar el servidor una vez conectado a la base de datos
         app.listen(port, () => {
             console.log(`Servidor iniciado en el puerto ${port}`);
         });
-    }).catch((error) => {
+    })
+    .catch((error) => {
         console.error('Error de conexión a MongoDB:', error);
     });
+
+// Registro de usuario
+app.post('/register', async (req, res) => {
+    const { nombre, apellidos, correo, nombreMC, pais, password } = req.body;
+    try {
+        // Verificar si el usuario ya existe
+        const existingUser = await User.findOne({ correo });
+        if (existingUser) {
+            return res.sendFile(path.join(__dirname, '/public/front/AdmUser/KO/registerKO.html'));
+        }
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            nombre,
+            apellidos,
+            correo,
+            nombreMC,
+            pais,
+            password: hashedPassword,
+        });
+        await newUser.save();
+        res.redirect('/'); 
+    } catch (error) {
+        console.error("Error en el registro:", error);
+        res.status(500).send('Internal server error');
+    }
+});
+
+// Inicio de sesión
+app.post('/login', async (req, res) => {
+    const { correo, password } = req.body;
+    
+    // Verificar los datos que llegan
+    
+    if (!correo || !password) {
+        return res.status(400).send('Correo o contraseña no proporcionados');
+    }
+    
+    try {
+        // Buscar el usuario por su correo electrónico
+        const user = await User.findOne({ correo });
+        
+        // Verificar si el usuario existe y la contraseña es correcta
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.sendFile(path.join(__dirname, '/public/front/AdmUser/KO/loginKO.html')); 
+        }
+
+        res.redirect('/'); 
+    } catch (error) {
+        console.error('Error en el inicio de sesión:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
+
 
 // Rutas para servir archivos HTML estáticos
 app.get("/", (req, res) => {
@@ -53,7 +108,6 @@ app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/front/AdmUser/register/register.html'));
 });
 
-// Ruta para servir el formulario de agregar noticia
 app.get("/nuevanoticia", (req, res) => {
     res.sendFile(path.join(__dirname, 'public/back/insertNoticia/insertNoticia.html'));
 });
---
+++ modelos/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    nombre: { type: String, required: true },
+    apellidos: { type: String, required: true },
+    correo: { type: String, required: true },
+    nombreMC: { type: String, required: true },
+    pais: { type: String, required: true },
+    password: { type: String, required: true }
+
+});
+
+module.exports = mongoose.model('usuario', userSchema);
